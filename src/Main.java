218e1d6aea966a341ba180942c8dc6f6ab8bcea6
sumithreddy2022 <sumithreddy202@gmail.com>
2026-03-10 10:02:25 +0530

    UC5 - Stack-Based Palindrome Checker

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,37 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Reservation {
-    String guestName;
-    String roomType;
+        String guestName;
+        String roomType;
 
-    Reservation(String guestName, String roomType) {
-        this.guestName = guestName;
-        this.roomType = roomType;
-    }
-
-    void display() {
-        System.out.println("Guest: " + guestName + " | Room Type: " + roomType);
-    }
-}
-class Main {
-
-    public static void main(String[] args) {
-
-        Queue<Reservation> bookingQueue = new LinkedList<>();
-
-        bookingQueue.add(new Reservation("Alice", "Single Room"));
-        bookingQueue.add(new Reservation("Bob", "Double Room"));
-        bookingQueue.add(new Reservation("Charlie", "Suite Room"));
-
-        System.out.println("Book My Stay - Hotel Booking System");
-        System.out.println("Version 5.1");
-        System.out.println();
-        System.out.println("Booking Requests (First-Come-First-Served):");
-        System.out.println();
-
-        for (Reservation r : bookingQueue) {
-            r.display();
+        Reservation(String guestName, String roomType) {
+                this.guestName = guestName;
+                this.roomType = roomType;
         }
-    }
+
+        void display() {
+                System.out.println("Guest: " + guestName + " | Room Type: " + roomType);
+        }
+}
+
+public class Main {
+
+        public static void main(String[] args) {
+
+                Queue<Reservation> bookingQueue = new LinkedList<>();
+
+                bookingQueue.add(new Reservation("Alice", "Single Room"));
+                bookingQueue.add(new Reservation("Bob", "Double Room"));
+                bookingQueue.add(new Reservation("Charlie", "Suite Room"));
+
+                System.out.println("Book My Stay - Hotel Booking System");
+                System.out.println("Version 5.1");
+                System.out.println();
+                System.out.println("Booking Requests (First-Come-First-Served):");
+                System.out.println();
+
+                for (Reservation r : bookingQueue) {
+                        r.display();
+                }
+        }
 }
