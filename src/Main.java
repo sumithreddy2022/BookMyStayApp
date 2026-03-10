4d86a92b18ca1e9131b42f8155fd17fc7badb26b
sumithreddy2022 <sumithreddy202@gmail.com>
2026-03-10 09:36:01 +0530

    UC2 : Basic Room Types & Static Availability

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,65 @@
-class BookMyStayApp {
-    public static void main(String[] args) {
-        String appName = "Book My Stay - Hotel Booking System";
-        String version = "Version 1.0";
+abstract class Room {
+    String type;
+    int beds;
+    int size;
+    double price;
 
-        System.out.println("Welcome to " + appName);
-        System.out.println(version);
+    Room(String type, int beds, int size, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    void displayDetails() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sqm");
+        System.out.println("Price: Rs" + price);
+    }
+}
+
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 20, 1000);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 35, 1800);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 50, 3000);
+    }
+}
+
+ class BookMyStayApp {
+    public static void main(String[] args) {
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Book My Stay - Hotel Booking System");
+        System.out.println("Version 2.1");
+        System.out.println();
+
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println();
+
+        doubleRoom.displayDetails();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println();
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
